208cb4cf75a60568a6e11709e3d7d4ea372d902f
ipw0630 <indahpermataw@gmail.com>
2024-07-14 23:53:42 +0700

    update progress

--- docker-mysql/mysql-scripts/init.sql/ingest-pet-adoption.sql
+++
@@ -1,2 +0,0 @@
-COPY pet_adoption_data FROM '/data/pet_adoption_data.csv' DELIMITER AS ',' CSV HEADER;
-SELECT * FROM retail LIMIT 5;
--- docker-mysql/mysql-scripts/init.sql/init.sql
+++ docker-mysql/mysql-scripts/init.sql/init.sql
@@ -1,2 +1,20 @@
-SELECT 'CREATE DATABASE pet_adoption_db'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'pet_adoption_db')\gexec
+CREATE DATABASE IF NOT EXISTS pet_adoption_db;
+
+USE pet_adoption_db;
+
+CREATE TABLE IF NOT EXISTS pet_adoption (
+    PetID INT,
+    PetType VARCHAR(50),
+    Breed VARCHAR(100),
+    AgeMonths INT,
+    Color VARCHAR(50),
+    Size VARCHAR(50),
+    WeightKg DOUBLE,
+    Vaccinated INT,
+    HealthCondition INT,
+    TimeInShelterDays INT,
+    AdoptionFee INT,
+    PreviousOwner INT,
+    AdoptionLikelihood INT,
+    AgeCategory VARCHAR(20)
+);
